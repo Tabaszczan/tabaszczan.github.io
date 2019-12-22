2f64c0e01c6990d23a037277b7df3f4606b69ab3
PodolakM <mati.pooo@gmail.com>
2019-12-22 12:57:06 +0100

    Dodanie zawartosci

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.carousel').carousel('pause');
+    $('.alert').click(function () {
+
+        $.alert({
+            title: 'A witam!',
+            content: 'url:../text.txt',
+            contentLoaded: function (data, status, xhr) {
+                this.setContentAppend('<br>Status: ' + status);
+            },
+            autoClose: 'ok|8000',
+            draggable: true,
+            backgroundDismiss: false,
+            backgroundDismissAnimation: 'shake',
+            theme: 'my-theme',
+            closeIcon: true,
+            closeIconClass: 'fas fa-times',
+        });
+    });
 });
